946c1553b4883fabee7bcd01bf22b8f461757bd2
Raihan Islam <raihanislam.cse@gmail.com>
2024-07-12 15:50:59 +0600

    update code

---
+++ views/submenu.php
@@ -0,0 +1,17 @@
+<?php 
+echo plugin_dir_path( __FILE__ );
+echo "<br/>";
+echo PLUGIN_BASENAME;
+echo "<h2 class='sub'>Submenu</h2>";
+echo "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod saepe, 
+laborum temporibus tempore laboriosam laudantium reprehenderit rem
+ voluptate nemo blanditiis architecto, autem possimus harum provident
+  minus asperiores beatae sint repellat.</p>";
+
+  echo "<br/>";
+  echo "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod saepe, 
+laborum temporibus tempore laboriosam laudantium reprehenderit rem
+ voluptate nemo blanditiis architecto, autem possimus harum provident
+  minus asperiores beatae sint repellat.</p>";
+
+  echo "<br/>";
